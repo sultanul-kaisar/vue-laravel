dc43aa0dc4817d3f9c878aa9f5c59bedceb25f26
sultanul-kaisar <sultanulkaisar@gmail.com>
2023-05-09 20:03:57 +0600

    First Commit

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Welcome to Vue Js App</h1>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
---
+++ resources/js/components/mainapp.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>This is main component</h1>
+    </div>
+</template>
